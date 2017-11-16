ed9af493af9c5592b53f4eb4b1fd2972a612df76
박재현 <parkjaehyeon@ParkJaeHyun.local>
2017-11-16 14:37:17 +0900

    Change codes all of things

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-/* GET signup page. */
-router.get('/signup',function(req , res){
-  res.render("signup");
-});
-
 /* GET signin page. */
-router.get('/signin',function(req , res){
-  res.render("signin");
-});
+// router.get('/signin',function(req , res){
+// res.render("signin");
+// });
 
 
 module.exports = router;
