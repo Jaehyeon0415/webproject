687efd25035501b27bc30f64797c435257230882
박재현 <parkjaehyeon@ParkJaeHyun.local>
2017-11-22 04:09:36 +0900

    Change event backend

--- models/question.js
+++ models/question.js
@@ -3,18 +3,28 @@ const mongoosePaginate = require('mongoose-paginate');
 const Schema = mongoose.Schema;
 
 var schema = new Schema({
+  // 작성자
   author: { type: Schema.Types.ObjectId, ref: 'User' },
+  // 제목
   title: {type: String, trim: true, required: true},
+  // 상세 설명
   content1: {type: String, trim: true, required: true},
-  content2: [String],
+  //등록 조직 이름
   group_name: {type: String, trim: true, required: true},
+  // 등록 조직 설명
+  content2: [String],
+  // 날짜
   date: {type: String, trim: true, required: true},
+  // 장소
   locate:{type: String, trim: true, required: true},
   tags: [String],
+  // 이미지
   img: [String],
   numLikes: {type: Number, default: 0},
   numAnswers: {type: Number, default: 0},
   numReads: {type: Number, default: 0},
+  
+  // 작성 시간
   createdAt: {type: Date, default: Date.now}
 }, {
   toJSON: { virtuals: true},
--- routes/questions.js
+++ routes/questions.js
@@ -5,7 +5,7 @@ const catchErrors = require('../lib/async-error');
 
 const router = express.Router();
 
-// 동일한 코드가 users.js에도 있습니다. 이것은 나중에 수정합시다.
+// 동일한 코드가 users.js에도 있습니다.
 function needAuth(req, res, next) {
   if (req.isAuthenticated()) {
     next();
@@ -16,6 +16,7 @@ function needAuth(req, res, next) {
 }
 
 /* GET questions listing. */
+// event들이 나오는 페이지.
 router.get('/', catchErrors(async (req, res, next) => {
   const page = parseInt(req.query.page) || 1;
   const limit = parseInt(req.query.limit) || 10;
@@ -36,15 +37,78 @@ router.get('/', catchErrors(async (req, res, next) => {
   res.render('questions/index', {questions: questions, term: term, query: req.query});
 }));
 
+// 이벤트 작성
 router.get('/new', needAuth, (req, res, next) => {
   res.render('questions/new', {question: {}});
 });
 
+// 이벤트 post
+router.post('/save',needAuth,catchErrors(async (req , res ,next) =>{
+  // 이미지를 console.log로 출력해본다
+  var user_obj = await User.findById(req.user.id);
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files){
+      console.log(files);
+      // var url;
+      // const S3_Bucket = 'jibang';
+      // const img_name = files.roomimg.name;
+      const UserId = req.user.id;
+
+      // if(img_name != ''){
+      //     // 이미지 업로드
+      //     var s3 = new AWS.S3();
+      //     var params = {
+      //          Bucket: S3_Bucket,
+      //          Key:img_name,
+      //          ACL:'public-read',
+      //          Body: require('fs').createReadStream(files.roomimg.path)
+      //     }
+      //     s3.upload(params, function(err, data){
+      //          var result='';
+      //          if(err)
+      //             // result = 'Fail';
+      //             console.log(err);
+      //          else
+      //             console.log(data);
+      //     });
+      //     // 업로드 된 이미지 URL가지고 오기
+      //     url = `https://${S3_Bucket}.s3.amazonaws.com/${img_name}`;    
+      // }else{
+      //     url = "#";
+      // }
+      
+      // 방 내용 디비에 업로드
+      User.findById(UserId, function(err,user){
+          var newQuestion = Question({
+              author:user_obj,
+              title:fields.title,
+              content1:fields.content1,
+              group_name:fields.group_name,
+              content2:fields.content2,
+              date:fields.date,
+              locate:fields.locate,
+              img:url,
+              //img_key:img_name
+          });
+         
+      
+          newQuestion.save(function(err) {
+              if (err) {
+                  return next(err);
+              } else {
+                  res.redirect('/');
+              }
+          });
+      });
+  });
+}));
+// 회원정보 수정
 router.get('/:id/edit', needAuth, catchErrors(async (req, res, next) => {
   const question = await Question.findById(req.params.id);
   res.render('questions/edit', {question: question});
 }));
 
+// 후기 작성
 router.get('/:id', catchErrors(async (req, res, next) => {
   const question = await Question.findById(req.params.id).populate('author');
   const answers = await Answer.find({question: question.id}).populate('author');
@@ -54,6 +118,7 @@ router.get('/:id', catchErrors(async (req, res, next) => {
   res.render('questions/show', {question: question, answers: answers});
 }));
 
+// 
 router.put('/:id', catchErrors(async (req, res, next) => {
   const question = await Question.findById(req.params.id);
 
@@ -70,12 +135,14 @@ router.put('/:id', catchErrors(async (req, res, next) => {
   res.redirect('/questions');
 }));
 
+// 사용자 삭제
 router.delete('/:id', needAuth, catchErrors(async (req, res, next) => {
   await Question.findOneAndRemove({_id: req.params.id});
   req.flash('success', 'Successfully deleted');
   res.redirect('/questions');
 }));
 
+// 이벤트 생성
 router.post('/', needAuth, catchErrors(async (req, res, next) => {
   const user = req.user;
   var question = new Question({
